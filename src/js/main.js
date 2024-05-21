f2fa6de3d8d2b860b73b3d8240f2d81e65a912d7
ernesto-quispe <ernesto.cajan@gmail.com>
2024-05-21 14:28:04 -0400

    prelim css styling

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+const operationImages = document.querySelectorAll(".operationImage");
+const operationCheckedImages = document.querySelectorAll(".checked");
+
+
+operationImages.forEach((image) => image.addEventListener("click", (e) => {
+    
+    console.log(e);
+}))
